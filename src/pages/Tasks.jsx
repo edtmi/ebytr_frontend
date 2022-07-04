4ab84fb9c9388ca1bba59cf296c66ffa4b52c024
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-07-04 17:48:55 -0300

    :sparkles: creating page task

---
+++ src/pages/Tasks.jsx
@@ -0,0 +1,19 @@
+import Header from '../components/Header/Header';
+import Input from '../components/Input/Input';
+import Selects from '../components/Selects/Selects';
+import Checkbox from '../components/Checkbox/Checkbox';
+import Buttons from '../components/Buttons/Buttons';
+
+const Tasks = () => {
+  return (
+    <div>
+      <Header />
+      <Input />
+      <Selects />
+      <Checkbox />
+      <Buttons />
+    </div>
+  )
+};
+
+export default Tasks;
