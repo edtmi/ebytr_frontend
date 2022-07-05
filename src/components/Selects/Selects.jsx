b4fcc32fc83e7ce278d65e640d47fd0277fa1d6a
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-07-04 22:58:53 -0300

    :sparkles: call handleChange on change property

--- src/components/Selects/Selects.jsx
+++ src/components/Selects/Selects.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
+import TasksContext from '../../context/TasksContext';
+
 const Selects = () => {
+  const { handleChange } = useContext(TasksContext);
   return (
     <div>
-      <select name="statusTask">
+      <select
+        name="statusTask"
+        onC={ handleChange }
+      >
         <option>status</option>
         <option value="ready">pronto</option>
         <option value="progress">em andamento</option>
